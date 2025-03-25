908f75cceb58cdf44fe1bc6836e9643627cb88c6
Sumeet Bhangale <sumeetbhangale00@gmail.com>
2025-03-25 21:58:09 +0530

    Sum of n Natural Number

---
+++ SumOfNaturalNo.java
@@ -0,0 +1,19 @@
+package com.javaCodingProblems;
+
+public class SumOfNaturalNo {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+
+		for (int i = 1; i <= 5; i++) {
+
+			sum = sum + i;
+
+		}
+
+		System.out.println(sum);
+
+	}
+
+}
